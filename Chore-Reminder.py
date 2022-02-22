074ec718a7ffcc389292b891411f844e2f44f5a5
AngryGreenCrayon <angrygreencrayon@gmail.com>
2022-02-21 20:21:33 -0500

    Rev0 Initial concept and requirements

---
+++ Chore-Reminder.py
@@ -0,0 +1,21 @@
+#Python Project Idea: Chore Reminder
+#Summary: Program will send a reminder of day's chores
+
+#Inputs
+#-Chore and frequency
+#-Option for daily, weekly, or monthly occurance
+#-Input check (i.e. not 30 days in Feb)
+#-Command to print list of all chores in program (output list)
+#-Configuration of reminder time per day (i.e. 7pm during the week, 10 am during weekend)
+
+#Operational/implimentation ideas
+#Use calendar
+#Use system date/time
+
+#Outputs
+#-Chore reminder at designated frequency and time via desktop reminder
+#-include validation chore complete
+
+#Future features
+#-Weather impact - i.e. its going to rain when you take out the trash, do it early
+#-Accessed via network to check if household members did chore already
